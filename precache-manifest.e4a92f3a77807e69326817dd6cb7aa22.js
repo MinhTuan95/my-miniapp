291e7391dcc9647da50ba435b2ab383b6940c77a
MinhTuan95 <luongminhtuan4295@gmail.com>
2025-05-28 17:00:36 +0700

    Update base mini app

---
+++ precache-manifest.e4a92f3a77807e69326817dd6cb7aa22.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995b2ac011a0cd5c84472b57a3e3b156",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72fece5e4421a088c860",
+    "url": "/static/css/main.c080e719.chunk.css"
+  },
+  {
+    "revision": "f5992cd1618148692e24",
+    "url": "/static/js/2.3b95cbf2.chunk.js"
+  },
+  {
+    "revision": "d5fdc2cde9ddad10560909af38ec8a94",
+    "url": "/static/js/2.3b95cbf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72fece5e4421a088c860",
+    "url": "/static/js/main.d13fad2b.chunk.js"
+  },
+  {
+    "revision": "f2c16ce8af5e1a8c6084",
+    "url": "/static/js/runtime-main.0e768dda.js"
+  },
+  {
+    "revision": "7a89d3ee362ead4d31aa6c1900aaee85",
+    "url": "/static/media/road.7a89d3ee.gif"
+  },
+  {
+    "revision": "c6606fc4f02eae41405b0aa8105a7f56",
+    "url": "/static/media/road_infinite.c6606fc4.gif"
+  },
+  {
+    "revision": "825bfa3bb135130d890828d1416c60a9",
+    "url": "/static/media/road_webp.825bfa3b.gif"
+  }
+]);
